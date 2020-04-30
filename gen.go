8a774815615541980ec8bf2da83effc200a63088
robreid <rob.reid@lush.co.uk>
2020-04-30 12:04:49 +0100

    Fixed uuids

--- gen.go
+++ gen.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"crypto/rand"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/atotto/clipboard"
+	"github.com/google/uuid"
 
 	"github.com/spf13/cobra"
 )
@@ -33,14 +33,7 @@ func main() {
 }
 
 func guid(cmd *cobra.Command, args []string) {
-	b := make([]byte, 16)
-	_, err := rand.Read(b)
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
-	}
-
-	out := fmt.Sprintf("%x-%x-%x-%x-%x\n", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+	out := uuid.New().String()
 	fmt.Println(out)
 
 	if err := clipboard.WriteAll(out); err != nil {
